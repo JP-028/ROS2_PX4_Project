053a16e8336a919221eba8c906307eeafd0a8023
JP-028 <josephine.perc@gmail.com>
2026-05-11 18:15:53 +0200

    Update start_px4.sh
    make px4_sitl gz_x500   to PX4_SYS_AUTOSTART=4050 make px4_sitl gz_x500_dual_cam

--- scripts/start_px4.sh
+++ scripts/start_px4.sh
@@ -4,4 +4,4 @@ export DISPLAY=host.docker.internal:0
 export QT_X11_NO_MITSHM=1
 export LIBGL_ALWAYS_SOFTWARE=1
 export GZ_RENDER_ENGINE=ogre
-make px4_sitl gz_x500
+PX4_SYS_AUTOSTART=4050 make px4_sitl gz_x500_dual_cam
